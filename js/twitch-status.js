316311afb0eb72c7f08d83e3f9fe49cc3a66bb12
doppl3r <deben3@gmail.com>
2019-10-15 14:54:18 -0700

    add multiple links with asynchronous ajax calls

--- js/twitch-status.js
+++ js/twitch-status.js
@@ -1,22 +1,27 @@
 $(document).ready(function(){
     // initialize user login and status variables
     var login = '';
-    var twitchStatus = $('.twitch-status');
-    twitchStatus.html('<div class="icon"></div><div class="text">Offline</div>');
-    login = twitchStatus.attr('href').split('/');
-    login = login[3]; // get username from link
+    var twitchStatusLinks = $('.twitch-status');
 
-    // Use ajax to call Twitch API - Client ID: https://dev.twitch.tv/console/apps/create
-    $.ajax({
-        type: 'GET',
-        url: 'https://api.twitch.tv/helix/streams?user_login=' + login,
-        headers: { 'Client-ID': 'cwsaw2mxwu6k70kk3nxa13g5spxesx' },
-        success: function (data) {
-            console.log(data);
-            var status = "offline"; // default if data not null
-            if (data.data[0] != undefined) status = data.data[0]['type'];
-            twitchStatus.addClass(status);
-            twitchStatus.find('.text').text(status);
-        }
+    // loop through each link
+    twitchStatusLinks.each(function(index, value) {
+        var twitchStatusLink = $(this);
+        twitchStatusLink.html('<div class="icon"></div><div class="text">Offline</div>');
+        login = twitchStatusLink.attr('href').split('/');
+        login = login[3]; // get username from link
+
+        // Use ajax to call Twitch API - Client ID: https://dev.twitch.tv/console/apps/create
+        $.ajax({
+            type: 'GET',
+            url: 'https://api.twitch.tv/helix/streams?user_login=' + login,
+            headers: { 'Client-ID': 'cwsaw2mxwu6k70kk3nxa13g5spxesx' },
+            success: function (data) {
+                console.log(data);
+                var status = "offline"; // default if data not null
+                if (data.data[0] != undefined) status = data.data[0]['type'];
+                twitchStatusLink.addClass(status);
+                twitchStatusLink.find('.text').text(status);
+            }
+        });
     });
 });
--- readme.md
+++ readme.md
@@ -2,20 +2,26 @@
 
 Get the stream status of any user. Twitch API calls are controlled with jQuery ajax functionality.
 
-[**DEMO**](https://raw.githack.com/doppl3r/twitch-status/master/index.html)
+[**DEMO**](https://raw.githack.com/doppl3r/twitch-status/master/index.html) - See if shroud is online!
+
+## Installation
+
+  1. Include Javascript libraries (2) inside the `<body>`
+
+```
+<script src="js/jquery-3.4.1.min.js"></script>
+<script src="js/twitch-status.js"></script>
+```
+
+  2. Add link(s) with the streamer's user name. Must include `twitch-status` class
+
+```
+<a class="twitch-status" href="https://www.twitch.tv/shroud" target="_blank"></a>
+```
+
+  3. Your link(s) should correctly show "Offline" or "Online" if installed properly
 
 ## How to Create Client ID
 
   - Go to https://dev.twitch.tv/console/apps/create
   - Set up Name, OAuth Redirect URL (http://localhost), and Category
-
-## Example
-
-Simply change out the `href` to the twitch channel, and the status will appear on your website:
-
-```
-<a class="twitch-status" href="https://www.twitch.tv/kargoh" target="_blank">
-    <div class="icon"></div>
-    <div class="text"></div>
-</a>
-```
